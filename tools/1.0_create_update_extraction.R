59c8f846fbea73d2e2c4d5143a54cde7fec99d20
Adyan Rios <87143630+AdyanRios-NOAA@users.noreply.github.com>
2022-08-05 13:26:17 -0400

    indent subsections

--- tools/1.0_create_update_extraction.R
+++ tools/1.0_create_update_extraction.R
@@ -1,12 +1,12 @@
 # Create or update an earlier extraction ####
 
-# Load libraries
-librarian::shelf(here, tidyverse, ROracle, keyring, dotenv)
-
-# Run prep files
-source(file <- here("Tools", "0.1_create_folder_structure.R"))
-source(file <- here("Tools", "0.2_access_oracle.R"))
-source(file <- here("Tools", "0.3_create_functions.R"))
-
-# SEDAR 57U
-  cr_tip_sp(state_codes = c('PR', 'VI'), sp_codes = c(97648, 97646))
+  # Load libraries ####
+    librarian::shelf(here, tidyverse, ROracle, keyring, dotenv)
+  
+  # Run prep files ####
+    source(file <- here("Tools", "0.1_create_folder_structure.R"))
+    source(file <- here("Tools", "0.2_access_oracle.R"))
+    source(file <- here("Tools", "0.3_create_functions.R"))
+  
+  # SEDAR 57 ####
+    cr_tip_sp(state_codes = c('PR', 'VI'), sp_codes = c(97648, 97646))
