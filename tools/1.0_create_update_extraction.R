8b9f9db782cd4b0c9e9786ec1f1e96ec15837fee
Adyan Rios <87143630+AdyanRios-NOAA@users.noreply.github.com>
2022-08-08 10:30:08 -0400

    update to lowercase file structure

--- tools/1.0_create_update_extraction.R
+++ tools/1.0_create_update_extraction.R
@@ -4,9 +4,9 @@
     librarian::shelf(here, tidyverse, ROracle, keyring, dotenv)
   
   # Run prep files ####
-    source(file <- here("Tools", "0.1_create_folder_structure.R"))
-    source(file <- here("Tools", "0.2_access_oracle.R"))
-    source(file <- here("Tools", "0.3_create_functions.R"))
+    source(file <- here("tools", "0.1_create_folder_structure.R"))
+    source(file <- here("tools", "0.2_access_oracle.R"))
+    source(file <- here("tools", "0.3_create_functions.R"))
   
   # SEDAR 57U ####
     cr_tip_sp(state_codes = c('PR', 'VI'), sp_codes = c(97648, 97646))
