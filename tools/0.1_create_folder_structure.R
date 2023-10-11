9bac0b2cd1ebfb9474e3ec262cef3b74c0da09ea
Molly H. Stevens <molly.stevens@nmfs.local>
2023-10-11 14:07:02 -0400

    revert commit

--- tools/0.1_create_folder_structure.R
+++ tools/0.1_create_folder_structure.R
@@ -1,6 +1,3 @@
-##Load packages
-librarian::shelf(tidyverse, here)
-
 # Create output folders if they do not exist yet ####
   if (!dir.exists(here("data"))){ dir.create(here("data")) }
   if (!dir.exists(here("data", "raw"))){ dir.create(here("data", "raw")) }
