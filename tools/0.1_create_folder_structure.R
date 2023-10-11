9a15ae49df936207aea43eb0b247e9695376cca7
Molly H. Stevens <molly.stevens@nmfs.local>
2023-10-11 13:54:32 -0400

    load here, tidyverse packages

--- tools/0.1_create_folder_structure.R
+++ tools/0.1_create_folder_structure.R
@@ -1,3 +1,6 @@
+##Load packages
+librarian::shelf(tidyverse, here)
+
 # Create output folders if they do not exist yet ####
   if (!dir.exists(here("data"))){ dir.create(here("data")) }
   if (!dir.exists(here("data", "raw"))){ dir.create(here("data", "raw")) }
