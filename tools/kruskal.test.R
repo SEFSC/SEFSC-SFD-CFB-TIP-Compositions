376d5425bad8792ea9da2dc75fee7fb975fc900f
Adyan Rios <87143630+AdyanRios-NOAA@users.noreply.github.com>
2023-11-01 17:28:24 -0400

    example stat test to determine what gears are different

---
+++ tools/kruskal.test.R
@@ -0,0 +1,149 @@
+# https://www.r-bloggers.com/2022/05/how-to-perform-the-kruskal-wallis-test-in-r/#google_vignette
+
+# Load libraries ####
+librarian::shelf(here, tidyverse, car, ggpubr)
+
+tip_date <- max(
+  as.numeric(gsub(".*?([0-9]+).RDS*", "\\1",
+                  list.files(here("data", "raw"),
+                             pattern = "com_tip_PR_VI"))))
+
+tip_file <- list.files(here("data", "raw"),
+                       pattern = paste0("^com_tip_PR_VI_+", tip_date))
+
+
+tip <- readRDS(file = here("data", "raw", tip_file))
+
+unique(tip$COUNTY_LANDED)
+
+sttj_yt <- tip |>
+  filter(COUNTY_LANDED %in% c("ST THOMAS", "ST JOHN"),
+         OBS_STANDARD_SPECIES_CODE == "168907")
+
+sttj_gears <- sttj_yt |>
+  mutate(STANDARDGEARNAME_3 = case_when(STANDARDGEARNAME_3 != STANDARDGEARNAME_2 ~
+                                          STANDARDGEARNAME_3,
+                                        TRUE ~ NA_character_),
+         STANDARDGEARNAME_2 = case_when(STANDARDGEARNAME_2 != STANDARDGEARNAME_1 ~
+                                          STANDARDGEARNAME_2,
+                                        TRUE ~ NA_character_)) |>
+  group_by(STANDARDGEARNAME_1,
+         STANDARDGEARNAME_2,
+         STANDARDGEARNAME_3,
+         STANDARDGEARNAME_4,
+         STANDARDGEARNAME_5) |>
+  summarize(n = sum(QUANTITY, na.rm = TRUE),
+            min_year = min(YEAR),
+            max_year = max(YEAR),
+            years = n_distinct(YEAR),
+            n_year = round(n/years)) |>
+  arrange(desc(n_year)) |>
+  ungroup() |>
+  mutate(gear_id = row_number())
+
+sttj_gears
+
+
+sttj_yt |>
+  left_join(sttj_gears) |>
+  group_by(gear_id) %>%
+  summarise(
+    count = n(),
+    mean = mean(LENGTH1_MM, na.rm = TRUE),
+    sd = sd(LENGTH1_MM, na.rm = TRUE),
+    median = median(LENGTH1_MM, na.rm = TRUE),
+    IQR = IQR(LENGTH1_MM, na.rm = TRUE)
+  )
+
+test_gear_years = function(gear_a, gear_b) {
+  
+  prep_gears <- sttj_yt |>
+    left_join(sttj_gears) |> 
+    mutate(gear_id = as.factor(gear_id))
+  
+  years_a <- prep_gears |> filter(gear_id == gear_a) |> distinct(YEAR)
+  years_b <- prep_gears |> filter(gear_id == gear_b) |> distinct(YEAR)
+  years_ab <- pull(inner_join(years_a, years_b, by = "YEAR"), YEAR)
+  
+  use_gears <- prep_gears |>
+    filter(gear_id %in% c(gear_a, gear_b),
+           YEAR %in% years_ab)
+  
+  kt = kruskal.test(LENGTH1_MM ~ gear_id, data = use_gears)
+  
+  wt <- pairwise.wilcox.test(use_gears$LENGTH1_MM, use_gears$gear_id,
+                             p.adjust.method = "BH")
+  
+  box_years = ggboxplot(use_gears, x = "YEAR", y = "LENGTH1_MM",
+            color = "gear_id", palette = c("#00AFBB", "#E7B800"),
+            ylab = "LENGTH1_MM", xlab = "Gear",
+            orientation = "horizontal")
+  
+  box = ggboxplot(use_gears, x = "gear_id", y = "LENGTH1_MM",
+            color = "gear_id", palette = c("#00AFBB", "#E7B800"),
+            ylab = "LENGTH1_MM", xlab = "Gear")
+  
+  print(kt)
+  print(wt)
+  print(box_years)
+  print(box)
+  print(years_ab)
+}
+
+test_gear = function(gear_a, gear_b) {
+  
+  prep_gears <- sttj_yt |>
+    left_join(sttj_gears) |> 
+    mutate(gear_id = as.factor(gear_id))
+  
+  use_gears <- prep_gears |>
+    filter(gear_id %in% c(gear_a, gear_b))
+  
+  kt = kruskal.test(LENGTH1_MM ~ gear_id, data = use_gears)
+  
+  wt <- pairwise.wilcox.test(use_gears$LENGTH1_MM, use_gears$gear_id,
+                       p.adjust.method = "BH")
+  
+  box <- ggboxplot(use_gears, x = "gear_id", y = "LENGTH1_MM",
+            color = "gear_id", palette = c("#00AFBB", "#E7B800"),
+            ylab = "LENGTH1_MM", xlab = "Gear")
+  
+  density_plot <- ggdensity(use_gears, x = "LENGTH1_MM",
+            add = "mean", rug = TRUE,
+            color = "gear_id", fill = "gear_id",
+            palette = c("#00AFBB", "#E7B800"),
+            ylab = "LENGTH1_MM", xlab = "Gear")
+  
+  print(kt)
+  print(wt)
+  print(box)
+  print(density_plot)
+}
+
+# CHECK DIFFERNCE BETWEEN (8) POTS AND TRAPS, FISH and (31) POTS AND TRAPS, SPINY LOBSTER
+test_gear(8,31) #Not different
+test_gear_years(8,31) #Not different
+
+# CHECK DIFFERNCE BETWEEN (6) HAUL SEINES and (9) ENCIRCLING NETS (PURSE)
+test_gear(6,9) #Not different
+test_gear_years(6,9) #Not different
+
+# CHECK DIFFERNCE BETWEEN (1) LINES HAND & (15) ROD AND REEL
+test_gear(1,15) #Not different
+test_gear_years(1,15) #Not different
+
+# CHECK DIFFERNCE BETWEEN (1) LINES HAND & (15) ROD AND REEL
+test_gear(1,15) #Not different
+test_gear_years(1,15) #Not different
+
+# CHECK DIFFERNCE BETWEEN (1) LINES HAND & (2) LINES POWER TROLL OTHER
+test_gear(1,2) #Different
+test_gear_years(1,2) #Different
+
+# CHECK DIFFERNCE BETWEEN (1) LINES HAND & (2) LINES POWER TROLL OTHER
+test_gear(1,2) #Different
+test_gear_years(1,2) #Different
+
+# CHECK DIFFERNCE BETWEEN (1) LINES HAND & (11) REEL, ELECTRIC OR HYDRAULIC
+test_gear(1,11) #Different
+test_gear_years(1,11) #Not different
