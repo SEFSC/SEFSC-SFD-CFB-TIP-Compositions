70c40879e4d90959c94022bf1db82a8f7cba0d44
Katherine Godwin <katherine.godwin@nmfs.local>
2023-11-01 13:24:59 -0400

    added function link

--- tools/1.0_create_update_extraction.R
+++ tools/1.0_create_update_extraction.R
@@ -7,6 +7,7 @@
     source(file <- here("tools", "0.1_create_folder_structure.R"))
     source(file <- here("tools", "0.2_access_oracle.R"))
     source(file <- here("tools", "0.3_create_functions.R"))
+    source(file <- here("tools", "0.4_create_functions_pt2.R"))
   
   # SEDAR 57U ####
     cr_tip_sp(state_codes = c('PR', 'VI'), sp_codes = c(97648, 97646))
