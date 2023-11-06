0917f4f13b2b2826105e3c993e9e33d7f28aad3d
Katherine Godwin <katherine.godwin@nmfs.local>
2023-11-06 16:11:29 -0500

    site name comparison and lat/long updates

--- tools/historical_flagging/3.9_muni_code_consolidation.R
+++ tools/historical_flagging/3.9_muni_code_consolidation.R
@@ -1,7 +1,7 @@
 # muni code consolidation across projects
 
 # libraries
-librarian::shelf(here, tidyverse, ROracle, keyring, dotenv, lubridate, readr)
+librarian::shelf(here, tidyverse, ROracle, keyring, dotenv, lubridate, readr, stringr)
 
 # load csvs
 # edits before upload: remove accents on letters, convert degree to decimal coordinates - in process 
@@ -35,25 +35,45 @@ pr_area_comparisons_mer$MER_Name<- tolower(pr_area_comparisons_mer$MER_Name)
 pr_area_comparisons_mer$MER_PR_Region <- tolower(pr_area_comparisons_mer$MER_PR_Region)
 pr_area_comparisons_mer$MER_code <- tolower(pr_area_comparisons_mer$MER_code)
 
-# create common column name
+# CONVERT DMS TO DECIMAL DEGREES - COMPLETED WITHIN CSV
+# all longitudes should be negative
+
+# remove direction (N,W) from MER and add (-) to long 
+pr_area_comparisons_mer$MER_lat <- str_sub(pr_area_comparisons_mer$MER_lat, end = -2)  
+pr_area_comparisons_mer$MER_long <- str_sub(pr_area_comparisons_mer$MER_long, end = -2)  
+pr_area_comparisons_mer$MER_long = as.numeric(as.character(pr_area_comparisons_mer$MER_long)) 
+pr_area_comparisons_mer <- pr_area_comparisons_mer |> 
+  mutate(MER_LONGITUDE = MER_long*(-1)) |> 
+  rename(MER_LATITUDE = MER_lat)
+
+# convert all lat column to numeric
+pr_area_comparisons_mer$MER_LATITUDE = as.numeric(as.character(pr_area_comparisons_mer$MER_LATITUDE)) 
+
+# create common muni and site column names
 pr_historical <- pr_area_comparisons_historical%>%
   rename(MUNICIPIO = H_muni_name)
 
 pr_mer <- pr_area_comparisons_mer%>%
-  rename(MUNICIPIO = MER_Municipio)
+  rename(#MUNICIPIO = MER_Municipio,
+         SITE_NAME = MER_Name)
 
 pr_mrip <- pr_area_comparisons_mrip%>%
-  rename(MUNICIPIO = MRIP_City)
+  rename(#MUNICIPIO = MRIP_County,
+         SITE_NAME = MRIP_Site_Name)
 
 pr_tip <- pr_area_comparisons_tip%>%
   rename(MUNICIPIO = TIP_CNTY_NAME)
 
 # pair down to necessary columns 
-pr_mer_skeleton <- pr_mer[,c(1,2,3, 11, 12, 13, 22)]
-pr_mrip_skeleton <- pr_mrip[,c(1,2, 4, 5, 5, 6, 7, 8, 9, 10)]
+pr_mer_skeleton <- pr_mer[,c(1,2,3, 11, 12, 13, 24)]
+pr_mrip_skeleton <- pr_mrip[,c(1,2, 4, 5, 6, 7, 8, 9, 10)]
+
+# remove rows from MER that have na's as those locations are not used
+pr_mer_clean <- na.omit(pr_mer_skeleton)
 
 # try full join
-mer_mrip_combo <- full_join(pr_mer, pr_mrip, by = 'MUNICIPIO')
-
+mer_mrip_combo <- full_join(pr_mrip_skeleton, pr_mer_clean, by = 'SITE_NAME')
+# only one location with the same site name in both tables, figure out another way to cross reference
+# 
 
 
