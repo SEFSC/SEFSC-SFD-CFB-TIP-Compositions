8da3da12dcadd28bd7c08fc341e2f34ceab7f37b
Katherine Godwin <katherine.godwin@nmfs.local>
2024-02-05 17:25:05 -0500

    historical work

--- tools/flagging_historical/3.12_com_rec_combo.R
+++ tools/flagging_historical/3.12_com_rec_combo.R
@@ -5,3 +5,10 @@ librarian::shelf(here, tidyverse, ROracle, keyring,
 
 wilson_tip_municodes <- read_csv("data/CSVs/wilson_tip_municodes.csv")
 combo_rec_merge_municodes <- read_csv("data/CSVs/combo_rec_merge_municodes.csv")
+
+pr_tip_municodes <- wilson_tip_municodes |> 
+  janitor::clean_names()
+
+#' comine rec and com sites and map
+#' map just com sites 
+#' use wilsons municodes to convert historical place codes to compare to tip 
