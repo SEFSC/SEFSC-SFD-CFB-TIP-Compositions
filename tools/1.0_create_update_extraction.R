41af9c8ab1c922e23e3bc4d07ce6fa8ca7492c07
Adyan Rios <87143630+AdyanRios-NOAA@users.noreply.github.com>
2024-02-29 11:43:22 -0500

    Added lines for additional sedar specific extractions

--- tools/1.0_create_update_extraction.R
+++ tools/1.0_create_update_extraction.R
@@ -1,14 +1,21 @@
 # Create or update an earlier extraction ####
 
-  # Load libraries ####
-    librarian::shelf(here, tidyverse, ROracle, keyring, dotenv)
-  
-  # Run prep files ####
-    source(file <- here("tools", "0.1_create_folder_structure.R"))
-    source(file <- here("tools", "0.2_access_oracle.R"))
-    source(file <- here("tools", "0.3_create_functions.R"))
-  
-  # SEDAR 57U ####
-    cr_tip_sp(state_codes = c('PR', 'VI'), sp_codes = c(97648, 97646))
-    n_cr_tip(state_codes = c('PR', 'VI'))
-    
+# Load libraries ####
+librarian::shelf(here, tidyverse, ROracle, keyring, dotenv)
+
+# Run prep files ####
+source(file <- here("tools", "0.1_create_folder_structure.R"))
+source(file <- here("tools", "0.2_access_oracle.R"))
+source(file <- here("tools", "0.3_create_functions.R"))
+
+# SEDAR 57U ####
+cr_tip_sp(state_codes = c("PR", "VI"), sp_codes = c(97648, 97646))
+n_cr_tip(state_codes = c("PR", "VI"))
+
+# SEDAR 80 ####
+cr_tip_sp(state_codes = c("PR", "VI"), sp_codes = c(173139))
+
+# SEDAR 84 ####
+cr_tip_sp(state_codes = c("PR", "VI"), sp_codes = c(170867))
+cr_tip_sp(state_codes = c("PR", "VI"), sp_codes = c(168907))
+cr_tip(state_codes = c("PR", "VI"))
