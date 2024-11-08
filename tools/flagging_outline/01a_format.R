fe75de57636c635a8a55b1a2ff15a0f6d029fda4
Katherine Godwin <katherine.godwin@nmfs.local>
2024-11-08 15:52:22 -0500

    confidentiality issues

--- tools/flagging_outline/01a_format.R
+++ tools/flagging_outline/01a_format.R
@@ -51,6 +51,7 @@
 # Create gear variable 
       gear = case_when(
         land_standard_gear_name == "NOT CODED" ~ standardgearname_1,
+        is.na(land_standard_gear_name) ~ standardgearname_1, 
         TRUE ~ land_standard_gear_name
       ),
 # Create area grid variable 
@@ -147,10 +148,20 @@
       year < (max_year+1) & year >= min_year
     )
   
+# figure out na gears
+  tip_spp_na <- tip_spp |> 
+    filter(is.na(gear))
+  
 # check gears
   unique(tip_spp$gear)  
 # replace "," with ";"
-  tip_spp$gear <- str_replace(tip_spp$gear, ",", ";")  
+  tip_spp$gear <- str_replace(tip_spp$gear, ",", ";") 
+# replace "NA" with "NOT CODED" 
+  tip_spp <- tip_spp |> 
+    mutate(gear =
+           as.character(ifelse(is.na(gear),
+                               "NOT CODED",
+                               gear)))
 # check gears
   unique(tip_spp$gear)
 
@@ -182,8 +193,125 @@
   # tip_range <- tip_nmfs[with(tip_nmfs, order(length2_mm)), ]
   # tip_range$length2_mm[1:25]
   
+  
+# conf by year and gear 
+  tip_count_a <- tip_nmfs |> 
+    group_by(island, date, year, gear) |> 
+    mutate(n_ID = n_distinct(sampling_unit_id) ) |> 
+    ungroup() |> 
+    group_by(island, year, gear) |> 
+    summarize(max_c = max(n_ID), 
+              .groups = "drop") |> 
+    filter(max_c <= 2) |> 
+    mutate(isl_yr_gear_c = "TRUE") |> 
+    right_join(tip_nmfs, 
+               by = join_by(island, year, gear)) |> 
+    # replace_na(isl_yr_gear_c = "FALSE")
+    mutate(isl_yr_gear_c =
+             as.character(ifelse(is.na(isl_yr_gear_c),
+                                 "FALSE",
+                                 isl_yr_gear_c)))
+# summary stats
+    tip_sum_yr_gr <- tip_count_a |>
+      select(island, year, gear, isl_yr_gear_c) |>
+      distinct() |>
+      filter(isl_yr_gear_c == "TRUE")
+
+    table(tip_sum_yr_gr$island, tip_sum_yr_gr$year)
+    table(tip_count_a$isl_yr_gear_c)
+
+    table(tip_count_a$isl_yr_gear_c)/nrow(tip_count_a)
+
+
+  
+# conf by gear 
+  tip_count_b <- tip_count_a |> 
+    group_by(island, date, gear) |> 
+    mutate(n_ID = n_distinct(sampling_unit_id) ) |> 
+    ungroup() |> 
+    group_by(island, gear) |> 
+    summarize(max_c = max(n_ID), 
+              .groups = "drop") |> 
+    filter(max_c <= 2) |> 
+    mutate(isl_gear_c = "TRUE") |> 
+    right_join(tip_count_a, 
+               by = join_by(island, gear)) |> 
+    # replace_na(isl_yr_gear_c = "FALSE")
+    mutate(isl_gear_c =
+             as.character(ifelse(is.na(isl_gear_c),
+                                 "FALSE",
+                                 isl_gear_c)))  
+  
+# summary stats   
+  tip_sum_yr <- tip_count_b |> 
+    select(island, gear, isl_gear_c) |> 
+    distinct() |> 
+    filter(isl_gear_c == "TRUE")
+  
+  table(tip_sum_yr$gear,tip_sum_yr$island )
+  table(tip_count_b$isl_gear_c)
+  table(tip_sum_yr$island )
+  table(tip_count_b$isl_gear_c)/nrow(tip_count_b)
+  
+# conf by gear 
+  tip_count_c <- tip_count_a |> 
+    group_by(island, date, gear) |> 
+    mutate(n_ID = n_distinct(vessel_id) ) |> 
+    ungroup() |> 
+    # group_by(island, gear) |> 
+    # summarize(max_c = max(n_ID), 
+    #           .groups = "drop") |> 
+    filter(n_ID <= 2) |> 
+    mutate(vess_isl_gear_c = "TRUE") |> 
+    # right_join(tip_count_a, 
+    #            by = join_by(island, gear)) |> 
+    # replace_na(isl_yr_gear_c = "FALSE")
+    mutate(vess_isl_gear_c =
+             as.character(ifelse(is.na(vess_isl_gear_c),
+                                 "FALSE",
+                                 vess_isl_gear_c)))  
+# summary stats   
+  tip_sum_yr_vess <- tip_count_c |> 
+    select(island, gear, vess_isl_gear_c) |> 
+    distinct() |> 
+    filter(vess_isl_gear_c == "TRUE")
+  
+  table(tip_sum_yr_vess$gear,tip_sum_yr_vess$island )
+  # table(tip_count_b$isl_gear_c)
+  table(tip_sum_yr_vess$island )
+  # table(tip_count_b$isl_gear_c)/nrow(tip_count_b)
+  
+  
+# conf by gear 
+  tip_count_d <- tip_count_a |> 
+    group_by(island, date, gear) |> 
+    mutate(n_lic = n_distinct(license) ) |> 
+    ungroup() |> 
+    # group_by(island, gear) |> 
+    # summarize(max_c = max(n_ID), 
+    #           .groups = "drop") |> 
+    filter(n_lic <= 2) |> 
+    mutate(lic_isl_gear_c = "TRUE") |> 
+    # right_join(tip_count_a, 
+    #            by = join_by(island, gear)) |> 
+    # replace_na(isl_yr_gear_c = "FALSE")
+    mutate(lic_isl_gear_c =
+             as.character(ifelse(is.na(lic_isl_gear_c),
+                                 "FALSE",
+                                 lic_isl_gear_c)))  
+# summary stats   
+  tip_sum_yr_lic <- tip_count_d |> 
+    select(island, gear, lic_isl_gear_c) |> 
+    distinct() |> 
+    filter(lic_isl_gear_c == "TRUE")
+  
+  table(tip_sum_yr_lic$gear,tip_sum_yr_lic$island )
+  # table(tip_count_b$isl_gear_c)
+  table(tip_sum_yr_lic$island )
+  # table(tip_count_b$isl_gear_c)/nrow(tip_count_b)
+  
 # Select variables relevant to flagging investigation ####
-  tip_spp_relevant <- tip_nmfs |>
+  tip_spp_relevant <- tip_count_b |>
     select(
       sampling_program, # denotes who recorded data
       data_provider, # noaa office handling data 
@@ -203,6 +331,7 @@
       sex_name, # male, female, unknown, not sexed
       gear, # gear from land_standard_gear_name subbed w/ gear_1 when unavailable
       gear_group_code, # nmfs gear group code 
+      standardgearname_2, # check to see if second gear was recorded 
       length1_mm, # length1 converted by TIP system to mm 
       length_unit1, # original length unit
       length_type1, # original length type
@@ -214,7 +343,10 @@
       sample_condition, # weight type
       fishery, # denotes recreational or commercial
       fishery_mode, # fishing_mode value
+      isl_yr_gear_c, # confidentiality flag island-year-gear strata
+      isl_gear_c, # confidentiality flag island-gear strata 
     ) 
+  
 
 # Save formatted tip_spp ####
   saveRDS(
